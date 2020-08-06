1c01cf9d6c315dcd2c8649795b96c3380a0d31b1
taiki-fw <taiki_52@outlook.jp>
2020-08-06 23:27:31 +0900

    :sparkles:ブラックジャックが動作するクラスを作成

--- src/blackJack/BlackJackGame.java
+++ src/blackJack/BlackJackGame.java
@@ -4,32 +4,41 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class BlackJackGame {
-	public static void main(String[] str) {
-		Scanner stdin = new Scanner(System.in);
-		
+	static Scanner stdin = new Scanner(System.in);
+	public static void start() {
+		System.out.println("☆★☆★☆★☆★☆★ ブラックジャックへようこそ！ ☆★☆★☆★☆★☆★");
+		System.out.println("ゲームを開始します。");
 		// 山札の生成
 		Desk desk = new Desk();
-		
-		Player user = new Player("あなた");
-		Player dealer = new Player("ディーラー");
-		
-		user.initialDraw(desk);
-		dealer.initialDraw(desk);
-		
+
+		Player user = new User("あなた", desk);
+		Player dealer = new Dealer("ディーラー", desk);
+
 		String inputString = null;
+		System.out.println("あなたの現在の得点は" + user.score + "です。");
 		do {
-			System.out.println("さらに引きますか？");
+			System.out.println("カードを引きますか？引く場合はYを、引かない場合はNを入力して下さい。");
 			inputString = stdin.nextLine();
-			if(Objects.equals(inputString, "Y")) {
-				user.optionalCardDraw(desk);	
+			if (Objects.equals(inputString, "Y")) {
+				user.optionalCardDraw();
+				if (user.isBurst) {
+					System.out.println("バーストしました。");
+					break;
+				}
 			}
 		} while (Objects.equals(inputString, "Y"));
 		
-		while (dealer.score < 17) {
-			dealer.optionalCardDraw(desk);
+		if(!(user.isBurst)) {
+			dealer.optionalCardDraw();	
 		}
-		
-		System.out.println("User" + user.score);
-		System.out.println("Dealer" + dealer.score);
+
+		System.out.println(user.name + "の得点は" + user.score + "です。");
+		System.out.println(dealer.name + "の得点は" + dealer.score + "です。");
+
+		if (dealer.isBurst || (user.isBurst == false && user.score > dealer.score)) {
+			System.out.println("あなたの勝ちです。");
+		}else {
+			System.out.println("あなたの負けです。");
+		}
 	}
 }
