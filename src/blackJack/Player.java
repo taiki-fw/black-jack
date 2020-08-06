a4145f30b197e927ace78d9c7aa90cec67911beb
taiki-fw <taiki_52@outlook.jp>
2020-08-06 23:26:09 +0900

    :sparkles:Add Abstract Player Class
    UserとDealerの違いは、カードを引く部分のみなので、そのメソッドをabstractとしスーパークラスを作成

--- src/blackJack/Player.java
+++ src/blackJack/Player.java
@@ -2,37 +2,29 @@ package blackJack;
 
 import java.util.ArrayList;
 
-public class Player {
-	String name;
-	ArrayList<Card> hands = new ArrayList<Card>();
+public abstract class Player {
+	protected static final int BURST_POINT = 21;
+	
+	final String name;
+	private ArrayList<Card> hands = new ArrayList<Card>();
 	int score = 0;
 	boolean isBurst = false;
+	Desk desk;
 
-	Player(String playerName) {
+	Player(String playerName, Desk desk) {
 		this.name = playerName;
+		this.desk = desk;
 	}
-	
-	private void drawCard(Desk desk) {
+
+	protected String drawCard(Desk desk) {
 		Card drowCard = desk.drowCard();
 		hands.add(drowCard);
 		score += hands.get(hands.size() - 1).Point();
 		
-		System.out.println(name + "の引いたカードは" + drowCard.NumberString() + "です。");
+		return drowCard.NumberString();
 	}
 
-	void initialDraw(Desk desk) {
-		drawCard(desk);
-		drawCard(desk); //ディーラーの2枚目は不明
-		System.out.println(name + "の現在の得点は" + score + "です。");
-	}
+	abstract void initialDraw(Desk desk);
 
-	void optionalCardDraw(Desk desk) {
-		//　ディーラーとユーザーの動作が異なるのでここはオーバーライド出来るようにabstructクラスを作成
-		drawCard(desk); 
-		System.out.println(name + "の現在の得点は" + score + "です。");
-		
-		if (score > 21) {
-			isBurst = true;
-		}
-	}
+	abstract void optionalCardDraw();
 }
